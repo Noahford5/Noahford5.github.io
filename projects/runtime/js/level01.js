84703e8f68dce5bc5fa3690a48dad216fce33c41
Noahford5 <ford320@Stcharles.k12.la.us>
2022-12-06 17:09:57 +0000

    Runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -5,7 +5,7 @@ var level01 = function (window) {
     var draw = window.opspark.draw;
     var createjs = window.createjs;
 
-    window.opspark.runLevelInGame = function(game) {
+    window.opspark.runLevelInGame = function (game) {
         // some useful constants 
         var groundY = game.groundY;
 
@@ -13,7 +13,7 @@ var level01 = function (window) {
         // behavior of our game
         var levelData = {
             "name": "Robot Romp",
-            "number": 1, 
+            "number": 1,
             "speed": -3,
             "gameItems": [
                 { "type": "sawblade", "x": 400, "y": groundY },
@@ -27,16 +27,63 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+        function createSawBlade(x, y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 20;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);
+            var obstacleImage = draw.bitmap("img/sawblade.png");
+            sawBladeHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25
+            obstacleImage.y = -25
+        }
+        function createBird(x, y) {
+            var hitZoneSize = 5;
+            var birdHitZone = game.createObstacle(hitZoneSize);
+            birdHitZone.x = x;
+            birdHitZone.y = y;
+            game.addGameItem(birdHitzone);
+            var birdImg = draw.bitmap("img/Bird.png");
+            birdHitZone.addChild(birdImg);
+            birdImg.x = -5
+            birdImg.y = -5
+        }
+        var enemy = game.createGameItem("enemy", 25);
+        var redSquare = draw.rect(50, 50, "red");
+        redSquare.x = -25;
+        redSquare.y = -25;
+        enemy.addChild(redSquare);
+        enemy.x = 400;
+        enemy.y = groundY - 50;
+        game.addGameItem(enemy);
+        enemy.velocityX -= 1;
+        enemy.onPlayerCollision = function playerCollision() {
+            game.changeIntegrity(-10)
+        };
+        enemy.onProjectileCollision = function projectileCollision() {
+            game.increaseScore(100);
+            enemy.fadeOut();
+        };
 
-        
-        
-        
+
+        for (var i = 1000; i < 35000; i += 500) {
+            var posNum = Math.floor(Math.random()* 2); 
+            var height;
+            if (posNum === 1) {
+                height = 350;
+            } else {
+                height = 250;
+            }
+            createSawBlade(i, height);
+        }
         // DO NOT EDIT CODE BELOW HERE
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = level01;
